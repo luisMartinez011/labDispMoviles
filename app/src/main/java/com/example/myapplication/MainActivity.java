63ca13fdc7dd73fce7f90ef7c654fb60a0b908db
DanteDeLordran <lordran@lordran.dev>
2023-11-07 21:53:40 -0600

    Building login with sqlite

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,76 +1,59 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new DBHandler(MainActivity.this);
-
         EditText usernameEditText = findViewById(R.id.usernameBtn);
         EditText passwordEditText = findViewById(R.id.passwordBtn);
         Button loginButton = findViewById(R.id.loginBtn);
         Button registerButton = findViewById(R.id.buttonRegistrarse);
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = usernameEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
-//                Intent intent = new Intent(MainActivity.this, OrdenesDeCompras.class);
-//                                    startActivity(intent);
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
 
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    Intent intent = new Intent(MainActivity.this, OrdenesDeCompras.class);
-                                    startActivity(intent);
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(MainActivity.this, "Autenticacion falladas",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+                DBHandler dbHandler = new DBHandler(MainActivity.this);
 
-
-
+                Cursor cursor = dbHandler.getUserByEmail(email);
+                if (cursor.moveToFirst()) {
+                    String storedPassword = cursor.getString(cursor.getColumnIndex(DBHandler.PASSWORD_COL_USERS));
+                    // Check if the entered password matches the stored password
+                    if (password.equals(storedPassword)) {
+                        // Passwords match, login successful
+                        Intent intent = new Intent(MainActivity.this, OrdenesDeCompras.class);
+                        startActivity(intent);
+                    } else {
+                        // Passwords do not match, login failed
+                        Toast.makeText(MainActivity.this, "Autenticacion falladas", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    // User not found, login failed
+                    Toast.makeText(MainActivity.this, "Autenticacion falladas", Toast.LENGTH_SHORT).show();
+                }
+                cursor.close();
             }
         });
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                     Intent intent = new Intent(MainActivity.this, RegistroUsuarios.class);
                     startActivity(intent);
-
             }
         });
     }
